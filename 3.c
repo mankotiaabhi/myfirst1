d82c894bec507ba6a1f42699e09018b849a05120
mankotiaabhi <124875926+mankotiaabhi@users.noreply.github.com>
2023-02-09 15:22:30 +0530

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+
+int main() 
+{
+    
+    char s[25],d[25],e[25],a[25];
+    scanf("%s %s %s",&s,&d,&e);
+    scanf("%s",&a);
+    printf("Hello, World!\n");
+    printf("%s %s %s %s",s,d,e,a);
+    return 0;
+}
